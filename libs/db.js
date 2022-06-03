1dc816c2101ed8beee0d7ab39c8ef5b7dbeee1c6
Andrii Bulbuk <bulbuk.andriy@gmail.com>
2022-06-03 16:00:17 +0300

    init commit

---
+++ libs/db.js
@@ -0,0 +1,83 @@
+import { MongoClient } from 'mongodb';
+
+const USER_PASSWORD='zF27PvYx_rFJWaX';
+const URL=`mongodb+srv://kapusta:${USER_PASSWORD}@cluster0.ggxrpws.mongodb.net/?retryWrites=true&w=majority`;
+const DB_NAME='nodejs-homework-2';
+const COLLECTION_NAME='todos';
+
+export class MongoDatabaseClient {
+    constructor() {
+        this.client = new MongoClient(URL);
+        this.db = this.client.db(DB_NAME);
+        this.collection = this.db.collection(COLLECTION_NAME);
+    }
+
+    async connect() {
+        try {
+            await this.client.connect();
+            console.log('Successfully connected to mongo database');
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    async getEntries() {
+        try {
+            const entries = await this.collection.find({}).toArray();
+
+            console.log(`Entries have been successfully retrieved from '${COLLECTION_NAME}' collection`);
+
+            return entries;
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    async getEntry(query) {
+        try {
+            const entry = await this.collection.findOne(query);
+
+            console.log(`Entry with '${JSON.stringify(query)}' fields has been successfully retrieved from '${COLLECTION_NAME}' collection`);
+
+            return entry;
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    async insertEntry(entry) {
+        try {
+            const result = await this.collection.insertOne(entry);
+
+            console.log(`Entry '${JSON.stringify(entry)}' has been successfully added to '${COLLECTION_NAME}' collection`);
+
+            return result;
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    async updateEntry(query, update) {
+        try {
+            const entry = await this.collection.updateOne(query, { $set: update });
+
+            console.log(`Entry with fields '${JSON.stringify(query)}' has been successfully updated with '${JSON.stringify(update)}' object`);
+
+            return entry;
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    async deleteEntry(query) {
+        try {
+            const entry = await this.collection.deleteOne(query);
+
+            console.log(`Entry with fields '${query}' has been successfully deleted from '${COLLECTION_NAME}' collection`);
+
+            return entry;
+        } catch (err) {
+            console.log(err);
+        }
+    }
+}
---
+++ services/server.js
@@ -0,0 +1,78 @@
+import express from 'express';
+import { MongoDatabaseClient } from "../libs/db.js";
+import { ObjectId } from "mongodb";
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+const mongoClient = new MongoDatabaseClient();
+await mongoClient.connect();
+
+app.use(express.json());
+
+app.get('/', async (req, res) => {
+    try {
+        const result = await mongoClient.getEntries()
+        res.send(result);
+    } catch (err) {
+        res.sendStatus(500);
+    }
+});
+
+app.post('/create', async (req, res) => {
+    try {
+        const todo = req.body;
+
+        await mongoClient.insertEntry(todo);
+
+        res.send(todo);
+    } catch (err) {
+        res.sendStatus(500);
+    }
+});
+
+app.patch('/update/:id', async (req, res) => {
+    try {
+        const update = req.body;
+        const query = { _id: new ObjectId(req.params.id) };
+        console.log(query);
+        console.log(update);
+
+        const result = await mongoClient.updateEntry(query, update);
+
+        res.send(result)
+    } catch (err) {
+        res.sendStatus(500);
+    }
+});
+
+app.patch('/complete/:id', async (req, res) => {
+    try {
+        const query = { _id: new ObjectId(req.params.id) };
+
+        const todo = await mongoClient.getEntry(query);
+        const result = await mongoClient.updateEntry(query, { done: !todo.done });
+
+        res.send(result)
+    } catch (err) {
+        res.sendStatus(500);
+    }
+});
+
+
+app.delete('/delete/:id', async (req, res) => {
+    try {
+        const query = { _id: new ObjectId(req.params.id) };
+
+        const result = await mongoClient.deleteEntry(query);
+
+        res.send(result)
+    } catch (err) {
+        res.sendStatus(500);
+    }
+});
+
+app.listen(5000, () => {
+    console.log(`server running on http://localhost:${PORT}`);
+})
+
